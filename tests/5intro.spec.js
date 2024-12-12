97e4e69f3377135130cc03087a0e1b70c7be9882
Daria-22 <daria.kozak@3plearning.com>
2024-12-13 08:41:43 +1100

    Added flag to verify the match between the search and the result of search

--- tests/5intro.spec.js
+++ tests/5intro.spec.js
@@ -1,80 +1,117 @@
 
 
 // @ts-check
-const { test, expect } = require('@playwright/test');
-const { assert } = require('console');
-//imported modules  
-//{} because imported part of modules 
-test('test', async ({ page }) => {
-    await page.goto('https://wise.jobs/jobs');
-    await page.getByRole('button', { name: 'Team ' }).click();
-    await page.getByLabel('People').click();
-    await page.getByLabel('Product').click();
-    await page.getByRole('button', { name: 'Office ' }).click();
-    await page.getByLabel('Tallinn').click();
-    await page.getByRole('button', { name: 'Apply' }).click();
-  });
+ const { test, expect } = require('@playwright/test');
+ const { assert } = require('console');
+// //imported modules  
+// //{} because imported part of modules 
+// test('test1', async ({ page }) => {
+// await page.goto('https://wise.jobs/jobs'); //open the site
+// await page.getByRole('button', { name: 'Team ' }).click(); //click team
+// await page.getByLabel('People').click();
+// await page.getByLabel('Product').click();
+// await page.getByRole('button', { name: 'Office ' }).click();
+// await page.getByLabel('Tallin').click();
+// await page.getByRole('button', { name: 'Apply' }).click();
+// });
 
-test('test job search Tallinn', async ({ page }) => {
- 
-    ///part 1 - opening the site
-  await page.goto('https://wise.jobs/jobs');
-  await page.waitForTimeout(2000);
-
-  //part3 - clicking the city
-let startCitySearch = await page.locator("div[id='jobfilters'] div:nth-child(2) div:nth-child(1) h3:nth-child(1)");
-await startCitySearch.click();
-
-let cityTallin = await page.locator("//ul[@class='option-display-list']/li[@class='filter-option']/span[@aria-label='Tallinn']");
-let valueOfCity = await cityTallin.textContent();
-console.log("City selected", valueOfCity);
- // @ts-ignore
- await cityTallin.click(); ///can be passed as object
- // @ts-ignore
- //await cityTallin.click();
- // @ts-ignore
- //await city.click();
-
-let applyCitySelection = await page.locator('(//div[@class="Horizontal search-filters-widget__filter-list"]//ul/div[@class="apply-filters-btn-container"]//button[@class="apply-filters-btn btn"])[2]');
- // @ts-ignore
-console.log("Count of found locators:", applyCitySelection.count());
-
-///await page.waitForTimeout(1000); 
-///await applyCitySelection.click(); 
-///await page.waitForTimeout(1000); 
-
-
+test('test2 job search Sydney', async ({ page }) => {
+///part 1 open the site
+await page.goto('https://wise.jobs/jobs');
 
 /// part 2 - selecting roles
-await page.locator('(//div[@class="option-type-header"])[1]').click(); 
+await page.getByRole('button', { name: 'Team ' }).click(); //click team
+await page.getByLabel('People').click();
+//get text from option People
+let peopleContent = await page.getByLabel('People').textContent();
+await page.getByLabel('Product').click(); 
+//get test from option Product
+let productContent = await page.getByLabel('Product').textContent();
+console.log(peopleContent,productContent);
+
+// @ts-ignore
+let regexPeople = peopleContent.replace(/\(.*$/, "");
+console.log(regexPeople);
+
+// @ts-ignore
+let regexProduct = productContent.replace(/\(.*$/, "");
+console.log(regexProduct);
+
+
+// if separately select getByLabel('People') and getByLabel("Product") - then the code works, ^^^
+// if the below code is used - does not work WHY???
+
+//let selectionOfChoices = await page.$$('//div[@class="optionfiltergroup category-opened"]/ul/li//span[@class="filter-text"]');
+// for(let selection of selectionOfChoices){
+// let selectionValue = await selection.textContent();
+// //console.log("Text content:", selectionValue);
+// // work well up to here
+// await page.waitForTimeout(1500);
+// let regexp = /^P/;
+// // @ts-ignore
+// if (selectionValue && regexp.test(selectionValue)){
+// await selection.click();
+// console.log("Roles selected",selectionValue); 
+
+// work well up to here
+
+//part3 - clicking the city/team
+await page.waitForTimeout(3000);
+await page.getByRole('button', { name: 'Office' }).click();
+await page.waitForTimeout(1500);
+
+//await page.locator("span[aria-label='Tallinn'] span[class='filter-text']").click();
+await page.getByLabel('Tallinn').click();
+await page.getByRole('button', { name: 'Apply' }).click();
 await page.waitForTimeout(2000);
-let selectionOfChoices = await page.$$('//div[@class="optionfiltergroup category-opened"]/ul/li//span[@class="filter-text"]');
 
-if (selectionOfChoices.length === 0) {
-  console.log('No elements found!');
-  return;
-}
+//await page.locator('(//div[@class="Horizontal search-filters-widget__filter-list"]//ul/div[@class="apply-filters-btn-container"]//button[@class="apply-filters-btn btn"])[2]').click();
 
-for(let selection of selectionOfChoices){
-      
-let selectionValue = await selection.textContent();
-  //console.log("Text content:", selectionValue);
-  
-let regexp = /^P/;
-  // @ts-ignore
-  
-if (selectionValue && regexp.test(selectionValue)){
-     console.log("Roles selected",selectionValue);
-     await selection.click();
-  }
-}
  
-let applyRoleSelection = await page.locator("ul[id='b8f6b27d-b7ba-46f4-03ef-08dbfa50089d'] button[class='apply-filters-btn btn']");
-await applyRoleSelection.click();
+//  // @ts-ignore
+// console.log("Count of found locators:", applyCitySelection.count());
+
+// ///await page.waitForTimeout(1000); 
+// ///await applyCitySelection.click(); 
+// ///await page.waitForTimeout(1000); 
+
+let citiesFound = await page.$$("//div[@class='attrax-vacancy-tile__location-freetext attrax-vacancy-tile__item']");
+//console.log(typeof(citiesFound));
+//await page.waitForTimeout(4000);
+//console.log(citiesFound);
+
+for (let city of citiesFound){
+let cityValue = await city.textContent();
+//console.log(cityValue);
+const regex = /Tallinn/
+if (cityValue && regex.test(cityValue)){
+//console.log("The found result contains the correct city", regex);
+}
+}
+
+let teamFound = await page.$$('//div[@class="attrax-vacancy-tile__option-team-valueset attrax-vacancy-tile__item-valueset"]//p[@class="attrax-vacancy-tile__item-value"]');
+await page.waitForTimeout(4000);
+for (let team of teamFound){
+  // @ts-ignore
+await page.waitForTimeout(1500);
+  let valueTeam = await team.textContent();
+
+  //creating a check
+  let flag = true;
+  //console.log(valueTeam);  
+  // @ts-ignore
+  //console.log(valueTeam.trim());
+
+  if (!valueTeam.trim() == regexPeople || !valueTeam.trim() == regexProduct){
+    flag = false;
+    //console.log("The output matches the search"); 
+  }
+  await expect(flag).toBeTruthy(); //returns 
+  //await expect(flag).toBeFalsy();
+   
+}
 
 await page.waitForTimeout(4000);
-
-
 }
 
 );
