4e6035faceb0cf58353a1d9a86c32faec95c65f2
Daria-22 <daria.kozak@3plearning.com>
2025-03-01 15:04:15 +1100

    Intro18 added how to get number of records, works now

--- tests/18intro.spec.js
+++ tests/18intro.spec.js
@@ -1,16 +1,29 @@
-///log to site and verify thyat emails have gmail domain
+///Objective of test:
+/// go to site
+/// log in
+/// select Admin
+// check the number of records displayed
+// delete the record
+/// verify that the record was deleted
 
-//npx playwright test tests/15intro.spec.js --headed
+// to run
+//npx playwright test tests/18intro.spec.js --headed --project chromium
+//npx playwright test tests/18intro.spec.js --ui  
+
+
+
 // @ts-check
 const { test, expect } = require('@playwright/test');
 //imported modules  
 //{} because imported part of modules 
 
 test('test1', async ({ page }) => { 
-    //opening the link, grabbing text for login
+    // opening the link, grabbing text for login
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
     await page.waitForTimeout(3000);
-    //copy and trim login
+    
+    // work on copying and pasting login and password
+    // copy and trim login
     let grabLogin = await page.locator("//p[@class='oxd-text oxd-text--p'][1]");
     let textValueGrabLogin = await grabLogin.textContent();
     //console.log(textValueGrabLogin);
@@ -42,13 +55,27 @@ test('test1', async ({ page }) => {
     await page.getByRole('button').click();
     await page.waitForTimeout(3000);
     
-    //click admin button
+    //click Admin button
     //let adminButton = await page.locator("//div/ul/li[@class='oxd-main-menu-item-wrapper'][.='Admin']");
     await page.click("//div/ul/li[@class='oxd-main-menu-item-wrapper'][.='Admin']");
+    await page.waitForTimeout(2000);
 
+    // checkign the record found before deleting
     let recordFound = await page.locator('(//span[@class="oxd-text oxd-text--span"])[1]');
     let valueOfRecord = await recordFound.textContent();
-    console.log(recordFound);
+    console.log(valueOfRecord);
+
+    // deleting the first record
+    let checkboxFirst = await page.locator("(//input[@type='checkbox'])[@value='1']");
+    await checkboxFirst.click();
+    await page.waitForTimeout(2000);
+    await page.click("(//i[@class='oxd-icon bi-trash'])[1]");
+
+    //checking the record number after deleting
+
+    // assertion to confirm that found records before deleting is a bigger number
+    
+
 
 }
 
